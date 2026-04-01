9ab68200608e74297b3136ce2e3dec245dc4e630
deminearchiver <minearchiver@gmail.com>
2026-04-01 11:16:30 +0300

    Published package_info_ffi version 1.1.0.

--- packages/package_info_ffi/lib/src/package_info_android.dart
+++ packages/package_info_ffi/lib/src/package_info_android.dart
@@ -1,4 +1,5 @@
 import 'package:jni/jni.dart';
+import 'package:jni_flutter/jni_flutter.dart';
 import 'package:package_info_ffi/package_info_ffi_platform_interface.dart';
 
 import 'jni_bindings.dart' as jb;
@@ -16,23 +17,22 @@ class PackageInfoAndroid extends PackageInfoPlatform {
     PackageInfoPlatform.instance = PackageInfoAndroid();
   }
 
-  static PackageInfoData _packageInfo() => Jni.androidApplicationContext
+  static PackageInfoData _packageInfo() => androidApplicationContext
       .use(jb.PackageInfoPlugin.getPackageInfo)
       .use(_packageInfoFromNative);
 
-  static PackageInfoData _packageInfoFromNative(
-    jb.PackageInfoData object,
-  ) => .from(
-    appName: object.getAppName().toDartString(releaseOriginal: true),
-    packageName: object.getPackageName().toDartString(releaseOriginal: true),
-    version: object.getVersion().toDartString(releaseOriginal: true),
-    buildNumber: object.getBuildNumber().toDartString(releaseOriginal: true),
-    buildSignature:
-        object.getBuildSignature()?.toDartString(releaseOriginal: true) ?? "",
-    installerStore: object.getInstallerStore()?.toDartString(
-      releaseOriginal: true,
-    ),
-    installTime: .fromMillisecondsSinceEpoch(object.getInstallTime()),
-    updateTime: .fromMicrosecondsSinceEpoch(object.getUpdateTime()),
-  );
+  static PackageInfoData _packageInfoFromNative(jb.PackageInfoData object) =>
+      .from(
+        appName: object.appName.toDartString(releaseOriginal: true),
+        packageName: object.packageName.toDartString(releaseOriginal: true),
+        version: object.version.toDartString(releaseOriginal: true),
+        buildNumber: object.buildNumber.toDartString(releaseOriginal: true),
+        buildSignature:
+            object.buildSignature?.toDartString(releaseOriginal: true) ?? "",
+        installerStore: object.installerStore?.toDartString(
+          releaseOriginal: true,
+        ),
+        installTime: .fromMillisecondsSinceEpoch(object.installTime),
+        updateTime: .fromMicrosecondsSinceEpoch(object.updateTime),
+      );
 }
