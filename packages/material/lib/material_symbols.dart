b007d0a623c594a5457908e66c07227d530853a1
deminearchiver <minearchiver@gmail.com>
2026-04-22 09:19:31 +0300

    Fixed hiding non-existent exports from material_symbols_icons.

--- packages/material/lib/material_symbols.dart
+++ packages/material/lib/material_symbols.dart
@@ -1,8 +1,5 @@
 export 'package:material_symbols_icons/material_symbols_icons.dart'
     hide
-        IconDataOutlined,
-        IconDataRounded,
-        IconDataSharp,
         IconVariationDefaults,
         MaterialSymbolsBase,
         TwoToneVariation,
