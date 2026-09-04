c3026babbe5dd148d3588b7288290faf6de92f15
deminearchiver <83916788+deminearchiver@users.noreply.github.com>
2026-09-04 10:07:27 +0300

    Migrated to jnigen 1.0.0 in system_colors_android bindings generator.

--- packages/system_colors/system_colors_android/tool/jnigen.dart
+++ packages/system_colors/system_colors_android/tool/jnigen.dart
@@ -2,39 +2,42 @@ import 'dart:io';
 
 import 'package:jnigen/jnigen.dart';
 
-void main(List<String> args) async {
+Future<void> main() async {
   final packageRoot = Platform.script.resolve("../");
-  await generateJniBindings(
-    .new(
-      outputConfig: .new(
-        dartConfig: .new(
-          path: packageRoot.resolve("lib/src/jni_bindings.g.dart"),
-          structure: .singleFile,
-        ),
-      ),
-      androidSdkConfig: .new(
-        addGradleDeps: true,
-        androidExample: packageRoot.resolve("example").toFilePath(),
-      ),
+  final generator = JniGenerator(
+    input: .new(
       sourcePath: [packageRoot.resolve("android/src/main/kotlin")],
       classes: [
         "io.qzz.deminearchiver.system_colors_android.SystemColorsPlugin",
         "io.qzz.deminearchiver.system_colors_android.DynamicTonalPalette",
         "io.qzz.deminearchiver.system_colors_android.DynamicColorScheme",
       ],
-      generateStubs: false,
-      visitors: [_CompanionVisitor()],
+      androidSdk: .new(
+        addGradleDeps: true,
+        androidExample: packageRoot.resolve("example/"),
+      ),
     ),
+    output: .new(
+      dart: .new(
+        path: packageRoot.resolve("lib/src/jni_bindings.g.dart"),
+        structure: .singleFile,
+      ),
+      generateStubs: false,
+    ),
+    visitors: [_CompanionVisitor()],
   );
+  await generator.generate();
 }
 
-class _CompanionVisitor extends Visitor {
+final class _CompanionVisitor() extends Visitor {
+  this : super.base();
+
   @override
   void visitMethod(Method method) {
     // Exclude constructors because the generated code is quite unpleasant.
     // It has been decided to treat all constructors as internal.
     if (method.isConstructor) {
-      method.isExcluded = true;
+      method.isIncluded = false;
     }
   }
 
@@ -46,7 +49,7 @@ class _CompanionVisitor extends Visitor {
     if (exclude.any(
       (name) => field.name == name || field.originalName == name,
     )) {
-      field.isExcluded = true;
+      field.isIncluded = false;
     }
   }
 }
